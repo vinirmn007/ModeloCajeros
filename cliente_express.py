ce29db08a5efda98afb3166d20023dc682577180
Alexis Roman <alexis.roman@unl.edu.ec>
2025-10-24 11:05:08 -0500

    cliente express

--- cliente_express.py
+++ cliente_express.py
@@ -1,4 +1,11 @@
 from cliente import Cliente
+import random
 
 class ClienteExpres(Cliente):
-    pass
+    def __init__(self):
+        super().__init__() 
+        self.articulos = 0
+
+    def set_articulos(self):
+        #entre 1 y 10 artículos
+        self.articulos = random.randint(1, 10)
