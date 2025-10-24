d00f14599d831d3a41ccd945f2d24fbdfb700a95
Mateo18xd <mateovalentin2005tk@gmail.com>
2025-10-24 16:34:05 -0500

    Mateo

--- cliente.py
+++ cliente.py
@@ -1,2 +1,33 @@
-class Cliente:
-    pass
+import random
+
+class Cliente: #Mateo
+    def __init__(self):
+        self.articulos = 0
+        self.tiempo_escaneo = 0
+        self.tipo_pago = random.choice(["efectivo", "tarjeta"])
+
+    # Setters y getters
+    def set_articulos(self):
+        self.articulos = random.randint(1, 40)
+    
+    def set_t_escaneo(self, t_escaneo):
+        self.tiempo_escaneo = t_escaneo
+
+    def get_tipo_pago(self):
+        return self.tipo_pago
+
+    def get_articulos(self):
+        return self.articulos
+
+    def t_cobro(self):
+        if self.tipo_pago == "efectivo":
+            return 30
+        if self.tipo_pago == "tarjeta":
+            return 15
+        
+    # Cálculo del tiempo total del cliente
+    def calcular_tiempo_total(self):
+        self.set_articulos()
+        t_escaneo = self.articulos * self.tiempo_escaneo
+        t_cobro = self.t_cobro()
+        return t_escaneo + t_cobro
