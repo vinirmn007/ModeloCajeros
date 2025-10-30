9913264d5ef5d2e6ccf8f6f94d1fc6a07fe58fd6
Wilmer Pardo <wilmer.pardo@unl.edu.ec>
2025-10-29 20:37:18 -0500

    nomenclatura

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
-# Importamos 'render_template' para servir el HTML
-from flask import Flask, jsonify, request, render_template # <--- ¡LÍNEA MODIFICADA!
+from flask import Flask, jsonify, request, render_template 
 from flask_cors import CORS
-
-# Importamos tus clases y funciones exactas
 from caja import Caja
-
 from utilidad import generar_clientes
 
 app = Flask(__name__)
 CORS(app) 
 
-# --- ¡NUEVA RUTA AÑADIDA! ---
 @app.route('/')
 def index():
-    """Esta ruta servirá tu archivo simulacion.html"""
     return render_template('simulacion.html')
 # -----------------------------
 
 
 def ejecutar_simulacion_caja(cantidad, tipo_cajero="normal", tipo_cliente="normal"):
-    """
-    Esta función usa tus clases originales para simular una caja
-    y devolver los datos que el frontend necesita.
-    """
+    #usa las clases del backend para mostrar en el fronted
     caja_simulada = Caja("normal") 
     generar_clientes(caja_simulada, cantidad, tipo=tipo_cliente)
 
@@ -48,12 +39,14 @@ def simular():
     n2 = int(request.args.get('n2', 0))
     n3 = int(request.args.get('n3', 0))
     n4 = int(request.args.get('n4', 0))
+    #n5 = int(request.args.get('n5', 0))
     n_exp = int(request.args.get('n_exp', 0))
 
     tiempos1, arts1, total1 = ejecutar_simulacion_caja(n1, "normal") #cambiar a principiante
     tiempos2, arts2, total2 = ejecutar_simulacion_caja(n2, "normal")
     tiempos3, arts3, total3 = ejecutar_simulacion_caja(n3, "normal")
     tiempos4, arts4, total4 = ejecutar_simulacion_caja(n4, "normal")
+    #tiempos5, arts5, total5 = ejecutar_simulacion_caja(n5, "normal")
     tiempos_exp, arts_exp, total_exp = ejecutar_simulacion_caja(n_exp, "normal", "express")
     
     datos_simulacion = {
@@ -61,6 +54,7 @@ def simular():
         "caja2": {"tiempos": tiempos2, "articulos": arts2},
         "caja3": {"tiempos": tiempos3, "articulos": arts3},
         "caja4": {"tiempos": tiempos4, "articulos": arts4},
+        #"caja5": {"tiempos": tiempos5, "articulos": arts5},
         "caja_express": {"tiempos": tiempos_exp, "articulos": arts_exp}
     }
     
@@ -69,9 +63,10 @@ def simular():
         "Caja 2": total2,
         "Caja 3": total3,
         "Caja 4": total4,
+        #"Caja 5": total5,
         "Caja Express": total_exp,
     }
-
+    #enviar datos al frontend
     return jsonify({
         "plan_simulacion": datos_simulacion,
         "totales_backend": totales
